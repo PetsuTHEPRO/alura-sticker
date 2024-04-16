8186377210a2c6194113d28e5781f3508b2038a5
José Peterson Menezes Machado <78889539+PetsuTHEPRO@users.noreply.github.com>
2024-04-16 13:09:37 -0300

    improve

--- src/app/App.java
+++ src/app/App.java
@@ -6,36 +6,27 @@ import java.util.List;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-		
-		// Fazer uma conex�o Http e buscar os 250 filmes
-		//String url = "https://api.mocki.io/v2/549a5d8b";
-		//ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
-		
-		//String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
-		//ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
-		
-		String url = "http://localhost:8080/linguagens";
-		ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
-		
-		var http = new ClienteHttp();
-		String json = http.buscaDados(url);
-		
-		// Exibir e manipular os dados 
-		List<Conteudo> conteudos = extrator.extraiConteudos(json);
-		
-		var geradora = new GeradoraDeFigurinhas();
-		for (int i = 0; i < 3; i++) {
-			
-			Conteudo conteudo = conteudos.get(i);
-			InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-			String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
-			
-			geradora.cria(inputStream, nomeArquivo);
-			
-			System.out.println(conteudo.getTitulo());
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) {
+        String apiUrl = "http://localhost:8080/linguagens";
+        ClienteHttp httpClient = new ClienteHttp();
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
 
+        try {
+            String json = httpClient.buscaDados(apiUrl);
+            List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+            for (int i = 0; i < 3; i++) {
+                Conteudo conteudo = conteudos.get(i);
+                InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+                String outputFileName = String.format("saida/%s.png", conteudo.getTitulo());
+
+                geradora.cria(inputStream, outputFileName);
+                System.out.printf("%s%n%n", conteudo.getTitulo());
+            }
+        } catch (Exception e) {
+            System.err.println("Erro durante a execução do programa:");
+            e.printStackTrace();
+        }
+    }
 }
