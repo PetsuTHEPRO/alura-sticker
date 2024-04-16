9a3532872e42e9f7caae21fff9b93d236c80040b
José Peterson Menezes Machado <78889539+PetsuTHEPRO@users.noreply.github.com>
2024-04-16 13:09:40 -0300

    improve

--- src/app/ExtratorDeConteudoDaNasa.java
+++ src/app/ExtratorDeConteudoDaNasa.java
@@ -4,26 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo{
+public class NasaContentExtractor implements ContentExtractor {
 
-	public List<Conteudo> extraiConteudos(String json) {
-		
-		// Extrair só os dados que interessam (titulo, poster, classificação)
-		var parser = new JsonParser();
-		List<Map<String, String>> listaDeAtributos = parser.parse(json);
-	
-		List<Conteudo> conteudos = new ArrayList<>();
-		
-		for (Map<String, String> atributos : listaDeAtributos) {
-			
-			String titulo = atributos.get("title");
-			String urlImagem = atributos.get("url");
-			
-			var conteudo = new Conteudo(titulo, urlImagem);
-			
-			conteudos.add(conteudo);
-		}
-		
-		return conteudos;
-	}
+    @Override
+    public List<Content> extractContent(String json) {
+        // Extract only the relevant data (title, poster, rating)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> attributesList = parser.parse(json);
+
+        List<Content> contents = new ArrayList<>();
+
+        for (Map<String, String> attributes : attributesList) {
+            String title = attributes.get("title");
+            String imageUrl = attributes.get("url");
+
+            Content content = new Content(title, imageUrl);
+            contents.add(content);
+        }
+
+        return contents;
+    }
 }
+
